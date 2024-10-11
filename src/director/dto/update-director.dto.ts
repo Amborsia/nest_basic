d9ca60006c28630a9f01a4980bf7dd9e28130a13
Amborsia <ghdrms1220@naver.com>
2024-10-11 14:59:38 +0900

    Mapped Type 및 Serialization 작업

--- src/director/dto/update-director.dto.ts
+++ src/director/dto/update-director.dto.ts
@@ -7,17 +7,4 @@ import {
   IsString,
 } from 'class-validator';
 
-export class UpdateDirectorDto {
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  name?: string;
-  @IsNotEmpty()
-  @IsDateString()
-  @IsOptional()
-  dob?: Date;
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  nationality?: string;
-}
+export class UpdateDirectorDto extends PartialType(CreateDirectorDto) {}
